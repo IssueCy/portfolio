5e851886f5f56e4e8850b31b8f93a50c541f7e18
IssueCy <simtecapplications@web.de>
2025-03-08 14:46:34 +0100

    added animations, contact section, skills section, footer, link site

--- script.js
+++ script.js
@@ -1,9 +1,75 @@
-const copyLabel = document.getElementById('copyLabel').addEventListener('click', () => {
-    const content = '.merix_';
-
-    navigator.clipboard.writeText(content).then(() => {
-        alert("Username copied.");
-    }).catch((err) => {
-        console.error(err);
-    });
+//! index animation
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      observer.unobserve(entry.target);
+    }
+  });
 });
+
+const hiddenElements = document.querySelectorAll(".headline");
+hiddenElements.forEach((el) => observer.observe(el));
+
+//! copy DC username
+const copyLabel = document.getElementById('copyLabel');
+
+if (copyLabel) {
+  copyLabel.addEventListener('click', () => {
+      const content = '.merix_';
+  
+      navigator.clipboard.writeText(content).then(() => {
+          alert("Username copied.");
+      }).catch((err) => {
+          console.error(err);
+      });
+  });
+}
+
+//! footer
+const copyright = document.getElementById('copyrightDate');
+copyright.innerText = `2023 - ${new Date().getFullYear()} Simtec Applications©`;
+
+
+
+//! contact form
+const webhookUrl = "https://discord.com/api/webhooks/1347888979617382511/JuelW517soxz0OPwhKwnQno-OD1jKtNyhoBnt-aJOeTwXJkzuDEaTpn0obfws1xS37Vk";
+
+const feedbackForm = document.getElementById("feedbackForm");
+
+if (feedbackForm) {
+  feedbackForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+  
+    const username = document.getElementById("username").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+  
+    if (message.trim() === "") {
+      document.getElementById("statusMessage").innerText = "Please enter a message!";
+      return;
+    }
+  
+    fetch(webhookUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: `@everyone\nUsername:  ${username}\nSubject: ${subject}\nMessage: ${message}`,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          document.getElementById("statusMessage").innerText = "Message sent successfully!";
+          document.getElementById("feedbackForm").reset();
+        } else {
+          document.getElementById("statusMessage").innerText = "Failed to send the message.";
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        document.getElementById("statusMessage").innerText = "An error occurred.";
+      });
+  });
+}
